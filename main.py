b7fae382a44890997ffe8e435bab40a040db4576
1991CRX <160188535+1991CRX@users.noreply.github.com>
2024-07-08 16:10:05 -0400

    remove unnecessary line breaks
    \n\n -> \n

--- main.py
+++ main.py
@@ -11,56 +11,56 @@ REMOVAL_MESSAGE_SUBJECT = "LOW QUALITY IMAGE"
 
 REMOVAL_COMMENT = """
 # Post Removed: Low Quality Image
-\n\n
+\n
 ---
 **Hi /u/{author}, please follow the instructions below and submit a higher quality image in a new post:**  
-\n\n
+\n
 ---
-\n\n
+\n
 1. Export your resume as a [**PDF file**](https://www.adobe.com/uk/acrobat/resources/google-doc-to-pdf.html).  
-\n\n
+\n
 2. Convert it to a **[600 DPI](https://www.adobe.com/uk/creativecloud/photography/discover/dots-per-inch-dpi-resolution.html) PNG file** using https://www.cleverpdf.com/pdf-to-images. https://imgur.com/RxxYFQe     
-\n\n
+\n
 3. On **[DESKTOP (NEW.REDDIT)](https://new.reddit.com/r/{sub}/submit)**, insert the PNG file into a **[TEXT SUBMISSION](https://imgur.com/8iik4YP)**.  
-\n\n
+\n
 ---
-\n\n
+\n
 **Please do not:**  
-\n\n
+\n
 * Take a picture of your resume with your phone camera  
-\n\n
+\n
 * Take a screenshot of your resume  
-\n\n
+\n
 * Crop out your margins  
-\n\n
+\n
 * Upload a dark mode version of your resume  
-\n\n
+\n
 ---
 """
 
 REMOVAL_MESSAGE = """
 **please follow the instructions below and submit a higher quality image in a new post:**  
-\n\n
+\n
 ---
-\n\n
+\n
 1. Export your resume as a [**PDF file**](https://www.adobe.com/uk/acrobat/resources/google-doc-to-pdf.html).  
-\n\n
+\n
 2. Convert it to a **[600 DPI](https://www.adobe.com/uk/creativecloud/photography/discover/dots-per-inch-dpi-resolution.html) PNG file** using https://www.cleverpdf.com/pdf-to-images. https://imgur.com/RxxYFQe     
-\n\n
+\n
 3. On **[DESKTOP (NEW.REDDIT)](https://new.reddit.com/r/{sub}/submit)**, insert the PNG file into a **[TEXT SUBMISSION](https://imgur.com/8iik4YP)**.  
-\n\n
+\n
 ---
-\n\n
+\n
 **Please do not:**  
-\n\n
+\n
 * Take a picture of your resume with your phone camera  
-\n\n
+\n
 * Take a screenshot of your resume  
-\n\n
+\n
 * Crop out your margins  
-\n\n
+\n
 * Upload a dark mode version of your resume  
-\n\n
+\n
 ---
 """
 
