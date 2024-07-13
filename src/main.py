8e5eaed2e7e9a1acbd5cbf253b25944d81e3d418
Sina Atalay <sinatalay@hotmail.com>
2024-07-13 21:21:23 +0300

    use `src` folder

--- src/main.py
+++ src/main.py
@@ -9,59 +9,12 @@ MIN_IMAGE_WIDTH_PX = 3100
 NUM_POSTS_TO_PROCESS = 10
 REMOVAL_MESSAGE_SUBJECT = "LOW QUALITY IMAGE"
 
-REMOVAL_COMMENT = """
-# Post Removed: Low Quality Image
-\n
----
-**Hi /u/{author}, please follow the instructions below and submit a higher quality image in a new post.**  
-\n
----
-\n
-1. Export your resume as a [**PDF file**](https://www.adobe.com/uk/acrobat/resources/google-doc-to-pdf.html).  
-\n
-2. Convert it to a **[600 DPI](https://www.adobe.com/uk/creativecloud/photography/discover/dots-per-inch-dpi-resolution.html) PNG file** using https://www.cleverpdf.com/pdf-to-images. https://imgur.com/RxxYFQe     
-\n
-3. On **[DESKTOP (NEW.REDDIT)](https://new.reddit.com/r/{sub}/submit)**, insert the PNG file into a **[TEXT SUBMISSION](https://imgur.com/8iik4YP)**.  
-\n
----
-\n
-**Please do not:**  
-\n
-* Take a picture of your resume with your phone camera  
-\n
-* Take a screenshot of your resume  
-\n
-* Crop out your margins  
-\n
-* Upload a dark mode version of your resume  
-\n
----
+REMOVAL_COMMENT = """ 
+
 """
 
 REMOVAL_MESSAGE = """
-**please follow the instructions below and submit a higher quality image in a new post.**  
-\n
----
-\n
-1. Export your resume as a [**PDF file**](https://www.adobe.com/uk/acrobat/resources/google-doc-to-pdf.html).  
-\n
-2. Convert it to a **[600 DPI](https://www.adobe.com/uk/creativecloud/photography/discover/dots-per-inch-dpi-resolution.html) PNG file** using https://www.cleverpdf.com/pdf-to-images. https://imgur.com/RxxYFQe     
-\n
-3. On **[DESKTOP (NEW.REDDIT)](https://new.reddit.com/r/{sub}/submit)**, insert the PNG file into a **[TEXT SUBMISSION](https://imgur.com/8iik4YP)**.  
-\n
----
-\n
-**Please do not:**  
-\n
-* Take a picture of your resume with your phone camera  
-\n
-* Take a screenshot of your resume  
-\n
-* Crop out your margins  
-\n
-* Upload a dark mode version of your resume  
-\n
----
+
 """
 
 
@@ -71,7 +24,7 @@ def setup_logger() -> None:
         datefmt="%m-%d %I:%M",
         level=logging.INFO,
         handlers=[
-            logging.FileHandler("logs.txt", mode="a"),
+            logging.FileHandler("logs.log", mode="a"),
             logging.StreamHandler(),
         ],
     )
